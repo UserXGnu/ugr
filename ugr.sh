b5b6646080e5a791136fa32f5e7f3c274d2ef793
Gildásio Júnior <gildasio97@gmail.com>
2015-09-26 22:38:04 -0300

    Add help menu and options

--- ugr.sh
+++ ugr.sh
@@ -5,17 +5,30 @@ normal="\033[0m"
 banner="
 \nUGR - ${bold}U${normal}pdate ${bold}G${normal}it ${bold}R${normal}epository
 \nBy gjuniioor [gjuniioor\x40protonmail\x2Ech]\n"
-HELP="
-Usage:
-\n\t$BASH_SOURCE [options]
-\n\nOptions:
-\n\t
-"
+
 
 version="0.9"
 
 interactive_mode="n"
 
+help_menu() # show help message
+{
+	HELP="
+Usage:
+\n\t$BASH_SOURCE [options]
+\n\nOptions:
+\n\t-h\t\tHelp message
+\n\t-i\t\tSet interactive mode on (default=off)
+\n\t-l <file>\tUse a repositories location file list
+\n\t-p <path>\tSet a path to focus
+\n\t\t\tIf none argument was seted, default is \"-p current_dir\"
+"
+	echo -e $HELP
+}
+try()
+{
+	echo -e "Try: \n\t$BASH_SOURCE -h"
+}
 list() # use file with list
 {
 	for dir in `cat ${1}`
@@ -73,10 +86,34 @@ update() # update repository
 }
 
 echo -e $banner
-echo -e $HELP
 
-# interactive_mode="y" # test interactive mode on
+while getopts ":hil:p:" o;
+	do
+		case "${o}" in
+			h) 
+				help_menu 
+				exit 1
+				;;
+			i) interactive_mode="y" ;;
+			l) 
+				list $OPTARG
+				exit 0
+				;;
+			p) 
+				path $OPTARG
+				exit 0
+				;;
+			\?)
+				echo "Invalid option: -$OPTARG" >&2
+				try
+				exit 1
+				;;
+			:)
+				echo "Option -$OPTARG requires an argument." >&2
+				try
+				exit 1
+				;;
+		esac
+	done
 
-# path # test current path
-# path /home/gjuniioor/Documents/git/ # test in a path
-# list /tmp/test # test with file list
+path # in use ./$0 -i (execute in current path)
